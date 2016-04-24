6904e41f4d1da924c8ba880f30b494eeca9e9b52
jitcoder <sam@bytecodestudios.com>
2016-04-23 22:07:55 -0700

    gulpfile updates, package.json updates. additional reorganization

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const source = require('vinyl-source-stream');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const streamify = require('gulp-streamify');
+const fs = require('fs');
 
 gulp.task('build:radmvc',['build:modules'],function(){
     return browserify({
@@ -22,7 +23,29 @@ gulp.task('build:radmvc',['build:modules'],function(){
 gulp.task('build:modules',function(){
    return gulp.src('./src/**/*')
    .pipe(uglify())
-   .pipe(gulp.dest('./')) 
+   .pipe(gulp.dest('./'));
+});
+
+gulp.task('build:npm-package',['build:radmvc','build:package-json'],function(){
+    let tasks = [];
+    
+    tasks.push(gulp.src('./rad.js').pipe(gulp.dest('./radmvc/')));
+    tasks.push(gulp.src('./lib/**/*').pipe(gulp.dest('./radmvc/lib')));
+    tasks.push(gulp.src('./dist/radmvc.min.js').pipe(gulp.dest('./radmvc/dist')));
+    return Promise.all(tasks);
+});
+
+gulp.task('build:package-json',function(){
+    try{
+        let pkg = JSON.parse(fs.readFileSync('./package.json'));
+        delete pkg.devDependencies;
+        fs.writeFileSync('./radmvc/package.json',JSON.stringify(pkg));
+        return true;
+    }
+    catch(e){
+        console.log(e.message);
+        return false;
+    }
 });
 
 gulp.task('default',['build:radmvc'],function(){});
