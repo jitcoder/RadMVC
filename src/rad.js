72877440119265762ba88c1c8faf90f3e1e305bf
jitcoder <sam@bytecodestudios.com>
2016-05-02 21:46:21 -0700

    refactoring

--- src/rad.js
+++ src/rad.js
@@ -4,6 +4,60 @@ var Controller = require('./lib/controller');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Rad = (function(){
+    
+    var instances = {};
+    var controllers = {};
+    var controllerStates = {};
+    
+    //todo:add support to memoize arguments
+    function invoke(controller,method){
+        var args = [].slice.call(arguments, 2);
+        var instance = instances[controller];
+        var result = instances[method].apply(instances,args);
+        //todo: figure out way to do type check rather than using properties
+        //to determine whether result is a react component;
+        //note: babel determines by checking if object has a render method
+        if(result && result['$$typeof'] && result['props']){
+            ReactDOM.render(result,controllerStates.view);
+            controllerStates[controller].view = method;
+        }
+    }
+    
+    //add support to memoize arguments
+    function refresh(controller){
+        var args = [].slice.call(arguments, 1);
+        controllers[controller][controllerStates[controller].view].apply(instances[controller],args);
+    }
+    
+    function generateControllerInterface(controller){
+        var instance = new controllers[controller]();
+        var methods = Object.getOwnPropertyNames(controllers[controller].prototype);
+        var icontroller = {};
+        
+        controllerStates[controller] = {
+            element: document.querySelector('[controller="' + controllerName + '"]'),
+            view: 'index'
+        }
+        
+        for(var i = 0; i < methods.length; i++){
+            icontroller[methods[i]] = invoke.bind(instance,controller,methods[i]);
+        }
+        
+        icontroller.refresh = refresh.bind(null,controller);
+    }
+    
+    return {
+        Mode:Model,
+        AjaxMode:AjaxModel,
+        Controller:Controller,
+        Initialize:function(){
+            
+        }
+    }
+    
+})();
+
 var Rad = {
     Model:Model,
     AjaxModel:AjaxModel,
@@ -48,9 +102,7 @@ var Rad = {
     }
 };
 
-exports.Rad = Rad;
-exports.React = React;
-exports.ReactDOM = ReactDOM;
+module.exports = Rad;
 
 if(window){
     window.Rad = Rad;
